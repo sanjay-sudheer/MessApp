bfe8b608e4e97677c00c688dd2edf6624bb6c55a
sanjay-sudheer <sanjaysudheer2003@gmail.com>
2024-10-31 15:19:44 +0530

    admin manage users

--- backend/routes/inmateRoutes.js
+++ backend/routes/inmateRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const inmateController = require('../controllers/inmateController');
+const authAdminMiddleware = require('../middlewares/authAdminMiddleware');
 
 // Define the routes for the inmate model
-router.post('/add', inmateController.addInmate);
-router.get('/all', inmateController.getAllInmates);
+router.post('/add',authAdminMiddleware, inmateController.addInmate);
+router.get('/all',authAdminMiddleware, inmateController.getAllInmates);
 router.get('/:admissionNumber', inmateController.getInmateByAdmissionNumber);
-router.put('/:admissionNumber', inmateController.updateInmate);
+router.put('/:admissionNumber',authAdminMiddleware, inmateController.updateInmate);
 
 module.exports = router;
--- frontend/src/AdminEditing/adminEditingOptions.jsx
+++ frontend/src/AdminEditing/adminEditingOptions.jsx
@@ -1,32 +1,82 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import './adminEditingOptions.css'
-import deleteIcon from '../assests/delete.png'
-import addIcon from '../assests/add-user.png'
-import editIcon from '../assests/editing.png'
-import userProfilePic from '../assests/user-profile-icon-removebg-preview.png'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './adminEditingOptions.css';
+import deleteIcon from '../assets/delete.png';
+import addIcon from '../assets/add-user.png';
+import editIcon from '../assets/editing.png';
+import userProfilePic from '../assets/user-profile-icon-removebg-preview.png';
+
 export default function AdminEditingOptions() {
+  const [inmates, setInmates] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchInmates = async () => {
+      try {
+        const token = localStorage.getItem('adminToken');
+        const response = await fetch('https://messapp-ymg5.onrender.com/api/inmates/all', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch inmate details');
+        }
+
+        const data = await response.json();
+        setInmates(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+
+    fetchInmates();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className='outerDiv'>
       <div className='adminEditingheader'>
         <span>Edit Members</span>
-        <Link to='/AdminAddingSection'><img src={addIcon} alt="" title='add member'/></Link>
+        <Link to='/AdminAddingSection'>
+          <img src={addIcon} alt="" title='add member' />
+        </Link>
       </div>
       <div className="userListDiv">
-        <div className="userDetail">
-          <div className="userNamePictureDiv">
-            <img src={userProfilePic} alt="" className='userProfilePic'/>
-            <span className='userName'>Sanjay Sudheer 
-              <br />
-              <span className='userDescription'>11025-22</span>
-            </span>
+        {inmates.map((inmate) => (
+          <div key={inmate.admissionNumber} className="userDetail">
+            <div className="userNamePictureDiv">
+              <img src={userProfilePic} alt="" className='userProfilePic' />
+              <span className='userName'>
+                {inmate.name} 
+                <br />
+                <span className='userDescription'>{inmate.admissionNumber}</span>
+              </span>
+            </div>
+            <div className="editingIconSections">
+              <Link to={`/adminEditSection/${inmate.admissionNumber}`}>
+                <img src={editIcon} alt="" title='edit member' />
+              </Link>
+              <Link to={`/AdminEditing/${inmate.admissionNumber}`}>
+                <img src={deleteIcon} alt="" title='remove member' />
+              </Link>
+            </div>
           </div>
-          <div className="editingIconSections">
-            <Link to='/adminEditSection'><img src={editIcon} alt="" title='edit member'/></Link>
-            <Link to='/AdminEditing'><img src={deleteIcon} alt="" title='remove member'/></Link>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
