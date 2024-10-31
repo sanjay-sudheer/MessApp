8ee6b82f8798a65c4373a37bcdd98f03cf3e6e2a
sanjay-sudheer <sanjaysudheer2003@gmail.com>
2024-10-31 16:11:41 +0530

    update in admin user management

--- frontend/src/AdminEditing/adminEditingOptions.jsx
+++ frontend/src/AdminEditing/adminEditingOptions.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import './adminEditingOptions.css';
-import deleteIcon from '../assets/delete.png';
-import addIcon from '../assets/add-user.png';
-import editIcon from '../assets/editing.png';
-import userProfilePic from '../assets/user-profile-icon-removebg-preview.png';
+import React from 'react'
+import { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
+import './adminEditingOptions.css'
+import deleteIcon from '../assests/delete.png'
+import addIcon from '../assests/add-user.png'
+import editIcon from '../assests/editing.png'
+import userProfilePic from '../assests/user-profile-icon-removebg-preview.png'
 
 export default function AdminEditingOptions() {
   const [inmates, setInmates] = useState([]);
@@ -15,7 +16,7 @@ export default function AdminEditingOptions() {
     const fetchInmates = async () => {
       try {
         const token = localStorage.getItem('adminToken');
-        const response = await fetch('http://localhost:5000/api/inmates/all', {
+        const response = await fetch('https://messapp-ymg5.onrender.com/api/inmate/all', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
@@ -28,9 +29,10 @@ export default function AdminEditingOptions() {
         }
 
         const data = await response.json();
-        setInmates(data);
+        setInmates(data); // Set the data directly
         setLoading(false);
       } catch (error) {
+        console.error('Error fetching inmates:', error);
         setError(error.message);
         setLoading(false);
       }
@@ -39,39 +41,43 @@ export default function AdminEditingOptions() {
     fetchInmates();
   }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if (!inmates.length) return <div>No inmate data available.</div>;
 
   return (
     <div className='outerDiv'>
       <div className='adminEditingheader'>
         <span>Edit Members</span>
         <Link to='/AdminAddingSection'>
-          <img src={addIcon} alt="" title='add member' />
+          <img src={addIcon} alt="Add member" title='add member' />
         </Link>
       </div>
       <div className="userListDiv">
         {inmates.map((inmate) => (
           <div key={inmate.admissionNumber} className="userDetail">
             <div className="userNamePictureDiv">
-              <img src={userProfilePic} alt="" className='userProfilePic' />
+              <img src={userProfilePic} alt="Profile" className='userProfilePic' />
               <span className='userName'>
-                {inmate.name} 
+                {inmate.name}
                 <br />
-                <span className='userDescription'>{inmate.admissionNumber}</span>
+                <span className='userDescription'>Admission No: {inmate.admissionNumber}</span>
+                <br />
+                <span className='userDescription'>Room No: {inmate.roomNumber}</span>
+                <br />
+                <span className='userDescription'>Department: {inmate.department}</span>
+                <br />
+                <span className='userDescription'>Year: {inmate.year}</span>
+                <br />
+                <span className='userDescription'>Batch: {inmate.batch}</span>
               </span>
             </div>
             <div className="editingIconSections">
               <Link to={`/adminEditSection/${inmate.admissionNumber}`}>
-                <img src={editIcon} alt="" title='edit member' />
+                <img src={editIcon} alt="Edit" title='edit member' />
               </Link>
               <Link to={`/AdminEditing/${inmate.admissionNumber}`}>
-                <img src={deleteIcon} alt="" title='remove member' />
+                <img src={deleteIcon} alt="Delete" title='remove member' />
               </Link>
             </div>
           </div>
